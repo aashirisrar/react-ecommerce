06be29b34debd3a153e3ff084ef846e38de0c92d
Aashir Israr <63807168+aashirisrar@users.noreply.github.com>
2023-07-14 00:25:02 +0500

    final commit

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -24,7 +24,30 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  const contextValue = { cartItems, addToCart, removeFromCart };
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+
+    return totalAmount;
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    updateCartItemCount,
+    getTotalCartAmount,
+  };
 
   return (
     <ShopContext.Provider value={contextValue}>
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import CartItem from "./cart-item";
+import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div className="mt-5">
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} key={product.id} />;
+          }
+        })}
+      </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h3 className="mt-3">Your Cart is Empty!</h3>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
